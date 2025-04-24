98e655617e2b115988eb99310aae35239cf4c3ea
Mennaa6 <mennaabdelfattahhh@gmail.com>
2025-04-24 19:36:46 +0200

    Modified the homa page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,29 @@
+import Banner from "../components/Banner";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import Productsshow from "../components/Productsshow";
+import Services from "../components/Services";
+import Type from "../components/Type";
+import Header from "./../components/Header";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      {/* <Header />
+      <Banner />
+      <Categories />
+      <Type />
+      <Productsshow />
+      <Services />
+      <Footer /> */}
+      <Header />
+      <Banner />
+      <Categories />
+      <Type />
+      <Productsshow />
+      <Services />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
