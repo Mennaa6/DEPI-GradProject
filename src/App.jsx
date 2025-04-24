a653ef6b4b9a36dd65a17e9ced79fc8cea26522f
salma-elnahas <selnahas909@gmail.com>
2025-04-25 01:22:54 +0300

    Fixed bug in App.jsx and updated styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import NotFound from "./pages/NotFound";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import Home from "./pages/Home";
-import Header from "./components/Header";
-import Products from "./pages/Products";
-import Footer from "./components/Footer";
 import Cart from "./pages/Cart"
 import ProductProvider from './context/ProductContext'
 import Checkout from './pages/Checkout'
@@ -14,17 +7,10 @@ const App = () => {
   return (
     <div>
       <ProductProvider>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/products" element={<Products />} />
-          <Route path="*" element={<NotFound />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/checkout' element={<Checkout />} />
       </Routes>
-        <Footer />
         </ProductProvider>
      
     </div>
