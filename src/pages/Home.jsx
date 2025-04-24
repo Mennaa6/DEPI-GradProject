8e4a20b2b8c7265ac2d7ed0df41808664007e060
Mennaa6 <mennaabdelfattahhh@gmail.com>
2025-04-25 01:00:03 +0300

    added the footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,13 +8,6 @@ import Header from "./../components/Header";
 const Home = () => {
   return (
     <div>
-      {/* <Header />
-      <Banner />
-      <Categories />
-      <Type />
-      <Productsshow />
-      <Services />
-      <Footer /> */}
       <Header />
       <Banner />
       <Categories />
