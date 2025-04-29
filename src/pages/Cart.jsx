f90a8bf47e5a5cf2fc47ec420b359f9e44edab12
salma-elnahas <selnahas909@gmail.com>
2025-04-29 12:32:55 +0300

    updated cart,checkout,productcontext and added delivery terms

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,103 +1,77 @@
-import React, { useContext } from "react";
-import { ProductContext } from "../context/ProductContext";
+import React, { useContext } from 'react'
+import { ProductContext } from '../context/ProductContext';
 import { Button } from "@material-tailwind/react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+ 
 
-const Cart = () => {
-  const {
-    cartItems,
-    addToCart,
-    deleteFromCart,
-    increaseQuantity,
-    decreaseQuantity,
-  } = useContext(ProductContext);
+ 
+
+ 
+const Cart = () => { 
+  const { cartItems,setCartItems, addToCart, deleteFromCart, increaseQuantity, decreaseQuantity,moveTowishlist } = useContext(ProductContext);
   const navigate = useNavigate();
   const proceedTocheckout = () => {
-    if (cartItems.length === 0) {
+    if (cartItems.length === 0)
+    {
       alert("your cart is empty");
       return;
     }
-    navigate("/checkout");
-  };
+      navigate('/checkout');
+  }
+  const proceedTodeliTerms = () => {
+       navigate('/deliveryterms')
+  }
 
   return (
-    <div className="h-screen bg-[#E4E0E1] px-4 py-6">
-      <div className="flex flex-col lg:flex-row gap-6">
-        <div className="w-full lg:w-2/3 bg-white shadow-lg p-6 rounded-md">
-          <h1 className="text-xl md:text-2xl font-bold mb-4">YOUR CART</h1>
-          {cartItems.length === 0 ? (
-            <p className="italic text-center">Your cart is empty</p>
-          ) : (
-            cartItems.map((item) => (
-              <div
-                key={item.id}
-                className="flex flex-col md:flex-row gap-4 mb-6 border-b pb-4"
-              >
-                <img
-                  className="w-28 h-28 rounded-lg object-cover shadow-md"
-                  src={item.image}
-                  alt={item.title}
-                />
-                <div className="flex-1">
-                  <h2 className="font-semibold text-lg">{item.title}</h2>
-                  <p className="text-sm text-gray-600">
-                    Category: {item.category}
-                  </p>
-                  <div className="flex gap-3 text-xs mt-3">
-                    <button className="underline">Edit</button>
-                    <button
-                      className="underline text-red-600"
-                      onClick={() => deleteFromCart(item.id)}
-                    >
-                      Delete
-                    </button>
-                    <button className="underline">Move to Wishlist</button>
-                  </div>
-                </div>
-                <div className="flex items-start gap-4 mt-2 md:mt-0">
-                  <div className="border bg-white flex items-center gap-4 px-2 py-1 rounded-md">
-                    <button
-                      className="px-2"
-                      onClick={() => increaseQuantity(item.id)}
-                    >
-                      +
-                    </button>
-                    <span>{item.quantity}</span>
-                    <button
-                      className="px-2"
-                      onClick={() => decreaseQuantity(item.id)}
-                    >
-                      -
-                    </button>
-                  </div>
-                  <p className="font-semibold">{item.price} EGP</p>
-                </div>
+    <div className='h-screen bg-[#E4E0E1] px-4 py-6'>
+  <div className='flex flex-col lg:flex-row gap-6'>
+    <div className='w-full lg:w-2/3 bg-white shadow-lg p-6 rounded-md'>
+      <h1 className='text-xl md:text-2xl font-bold mb-4'>YOUR CART</h1>
+      {cartItems.length === 0 ? (
+        <p className='italic text-center'>Your cart is empty</p>
+      ) : (
+        cartItems.map((item) => (
+          <div key={item.id} className='flex flex-col md:flex-row gap-4 mb-6 border-b pb-4'>
+            <img className="w-28 h-28 rounded-lg object-cover shadow-md" src={item.image} alt={item.title} />
+            <div className='flex-1'>
+              <h2 className='font-semibold text-lg'>{item.title}</h2>
+              <p className='text-sm text-gray-600'>Category: {item.category}</p>
+              <div className='flex gap-3 text-xs mt-3'>
+                <button className='underline text-red-600' onClick={() => deleteFromCart(item.id)}>Delete</button>
+                <button className='underline ' onClick={()=>moveTowishlist(item.id)}>
+                  Move to Wishlist</button>
               </div>
-            ))
-          )}
-          <Button
-            className="mt-4 w-full md:w-auto bg-[#493628] hover:bg-[#AB886D] text-white"
-            onClick={() => proceedTocheckout()}
-          >
-            Proceed to Checkout
-          </Button>
-        </div>
+            </div>
+            <div className='flex items-start gap-4 mt-2 md:mt-0'>
+              <div className='border bg-white flex items-center gap-4 px-2 py-1 rounded-md'>
+                <button className='px-2' onClick={()=> increaseQuantity(item.id)}>+</button>
+                <span>{item.quantity}</span>
+                <button className='px-2' onClick={() => decreaseQuantity(item.id)}>-</button>
+              </div>
+              <p className='font-semibold'>{item.price} EGP</p>
+            </div>
+          </div>
+        ))
+      )}
+      <Button className='mt-4 w-full md:w-auto bg-[#493628] hover:bg-[#AB886D] text-white' onClick={()=>proceedTocheckout()}>Proceed to Checkout</Button>
+    </div>
 
-     <div className='w-full lg:w-1/3 bg-[#D6C0B3] shadow-md p-6 rounded-md'>
+    <div className='w-full lg:w-1/3 bg-[#D6C0B3] shadow-md p-6 rounded-md'>
       <h2 className='text-lg md:text-3xl font-bold text-black p-2 mb-4 text-center'>CHECKOUT</h2>
-      <p className='text-center text-sm mb-4'>By placing your order, you agree to the <span className='text-blue-600'>Delivery Terms</span></p>
+      <p className='text-center text-sm mb-4'>By placing your order, you agree to the <button className='text-blue-600 underline' onClick={()=>proceedTodeliTerms()}>Delivery Terms</button></p>
 
       <h3 className='font-bold text-lg mb-2'>ORDER SUMMARY:</h3>
       <div className='bg-white p-4 rounded-md shadow'>
-        <p className='flex justify-between mb-2'><span>Sub Total</span><span>{cartItems.map((item) => item.price * item.quantity).reduce((total,product)=>total+product ,0).toFixed(2)} EGP</span></p>
-        <p className='flex justify-between mb-2'><span>Delivery</span><span>50</span></p>
+        <p className='flex justify-between mb-2'><span>Sub Total</span><span>{cartItems.reduce((total,item)=>total+(item.price * item.quantity) ,0).toFixed(2)} EGP</span></p>
         <hr className='my-3' />
-        <p className='flex justify-between font-semibold text-lg'><span>Total</span><span>{cartItems.map((item) => item.price * item.quantity).reduce((total,product)=>total+product+50 ,0).toFixed(2)}  EGP</span></p>
-        <Button className='w-full mt-4 bg-[#493628] hover:bg-[#AB886D]' onClick={()=>proceedTocheckout()}>Checkout</Button>
-        </div>
+         <Button className='w-full mt-4 bg-[#493628] hover:bg-[#AB886D]' onClick={()=>proceedTocheckout()}>Checkout</Button>
       </div>
-     </div></div>
-  );
-};
+    </div>
+  </div>
+</div>
 
-export default Cart;
+        
+  )
+}
+
+export default Cart
