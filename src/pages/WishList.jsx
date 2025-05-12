3c660d61e51cc31e7e0241eabd9464e70f625f11
Yusif892 <yk2385501@gmail.com>
2025-05-12 05:24:41 +0300

    add to fav is done

--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -1,60 +1,27 @@
-import { FaStar, FaRegHeart } from "react-icons/fa";
-import { MdAddShoppingCart, MdOutlineRemoveRedEye } from "react-icons/md";
+import ProductCard from "../components/ProductCard";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import React, { useState, useEffect, useContext } from "react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-  Button,
-  list,
-} from "@material-tailwind/react";
+import { Typography } from "@material-tailwind/react";
 import { Spinner } from "@material-tailwind/react";
-import SingleProduct from "../components/SingleProduct";
-import { ProductContext } from '../context/ProductContext';
-
 
 const Wishlist = () => {
-  // const [products, setProducts] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [openDialog, setOpenDialog] = useState(false);
-  const { products, wishlistItems, loading } = useContext(ProductContext)
-  const wishlistProducts = products.filter(product =>wishlistItems.includes(product._id));
 
   // const navigate = useNavigate();
 
-  // const fetchData = () => {
-  //   const userId = JSON.parse(window.localStorage.getItem("id"));
-  //   fetch(`https://spotted-thankful-mambo.glitch.me/users/${userId}`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       const listItems = data.wishlist;
-  //       fetch("https://spotted-thankful-mambo.glitch.me/products")
-  //         .then((res) => res.json())
-  //         .then((productData) => {
-  //           const allProducts = [
-  //             ...productData.men,
-  //             ...productData.women,
-  //             ...productData.accessories,
-  //           ];
-  //           const userProducts = [];
-  //           listItems.forEach((productId) => {
-  //             let product = allProducts.find(
-  //               (productItem) => productItem.id == productId
-  //             );
-  //             userProducts.push(product);
-  //             setProducts(userProducts);
-  //           });
-  //           console.log(userProducts);
-  //         });
-  //       setLoading(false);
-  //     })
-  //     .catch((e) => console.log(e.message));
-  // };
+  const fetchData = () => {
+    // const userId = JSON.parse(window.localStorage.getItem("id"));
+    const userId = "681cbcdb4fb78a0c071e023a";
+    fetch(`https://depis3.vercel.app/api/wishlist/${userId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.wishlist);
+        setLoading(false);
+      });
+  };
 
   useEffect(() => {
     // if (!window.localStorage.getItem("id")) {
@@ -69,18 +36,8 @@ const Wishlist = () => {
     });
     AOS.refresh();
 
-    // fetchData();
+    fetchData();
   }, []);
-
-  const handleProductClick = (product) => {
-    setSelectedProduct(product);
-    setOpenDialog(true);
-  };
-
-  const handleCloseDialog = () => {
-    setOpenDialog(false);
-    setSelectedProduct(null);
-  };
 
   if (loading) {
     return (
@@ -100,87 +57,14 @@ const Wishlist = () => {
         Wishlist
       </h1>
       <div className=" grid lg:grid-cols-3 grid-cols-1 justify-center items-center lg:gap-10 gap-5 m-5 md:grid-cols-2">
-        {wishlistProducts && wishlistProducts.length > 0 ? (
-          wishlistProducts.map((item) => (
-            <Card
-              data-aos="zoom-in"
-              data-aos-delay="100"
-              key={item._id}
-              className="w-full relative "
-            >
-              <CardHeader shadow={false} floated={false} className="h-96 ">
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className="h-full w-full object-cover"
-                />
-                <div
-                  id="icons"
-                  className="flex w-full justify-center items-center gap-3 absolute top-[1px]"
-                >
-                  <div
-                    className="bg-buttonColor hover:bg-hoverColor hover:text-black
-              rounded-full p-2 text-white cursor-pointer"
-                    onClick={() => handleProductClick(item)}
-                  >
-                    <MdOutlineRemoveRedEye />
-                  </div>
-                  <div
-                    className="bg-buttonColor hover:bg-hoverColor hover:text-black
-              rounded-full p-2 text-white cursor-pointer"
-                  >
-                    <FaRegHeart />
-                  </div>
-                  <div
-                    className="bg-buttonColor hover:bg-hoverColor hover:text-black
-              rounded-full p-2 text-white cursor-pointer"
-                    onClick={() => handleProductClick(item)}
-                  >
-                    <MdAddShoppingCart />
-                  </div>
-                </div>
-              </CardHeader>
-              <CardBody>
-                <div className="mb-2 flex items-center justify-between">
-                  <Typography color="blue-gray" className="font-semibold">
-                    {item.name}
-                  </Typography>
-                  <Typography color="blue-gray" className="font-medium">
-                    ${item.price}
-                  </Typography>
-                </div>
-                <Typography
-                  variant="small"
-                  color="gray"
-                  className="font-normal opacity-75"
-                >
-                  {item.description}
-                </Typography>
-              </CardBody>
-              <CardFooter className="pt-0">
-                <Button
-                  ripple={false}
-                  fullWidth={true}
-                  className="bg-buttonColor  hover:bg-hoverColor text-white shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
-                  onClick={() => handleProductClick(item)}
-                >
-                  Add to Cart
-                </Button>
-              </CardFooter>
-            </Card>
+        {products && products.length > 0 ? (
+          products.map((item, index) => (
+            <ProductCard product={item} key={index} />
           ))
         ) : (
           <Typography className="text-center">Loading products...</Typography>
         )}
       </div>
-      {/* Render SingleProduct dialog */}
-      {selectedProduct && (
-        <SingleProduct
-          open={openDialog}
-          handleClose={handleCloseDialog}
-          product={selectedProduct}
-        />
-      )}
     </div>
   );
 };
